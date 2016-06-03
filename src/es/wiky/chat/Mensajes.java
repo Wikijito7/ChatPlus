9c9e6c0cb2dad4945b08cf526819292a693642e2
Wikijito7 <wikyfg@gmail.com>
2016-06-03 15:21:12 +0200

    Some formatting

--- src/es/wiky/chat/Mensajes.java
+++ src/es/wiky/chat/Mensajes.java
@@ -8,11 +8,8 @@ public class Mensajes {
 	public static String nick = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.nick"));
 	public static String nick_change_usage = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.nick_change_usage"));
 	public static String successful_change_nick = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.successful_change_nick"));
-	//public static String new_nick = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.new_nick"));
 	public static String nick_changed = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.nick_changed"));
 	public static String player_dont_exist = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.player_dont_exist"));
-	//public static String player_set = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.player_set"));
-	//public static String player_set_reciever = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang." ).replaceAll("%NICK%", Main.config.getString("Users." + pl.getName() + ".cnick" )));
 	public static String playerSetReciever(String name){
 		return ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.player_set_reciever").replaceAll("%NICK%", Main.config.getString("Users." + name + ".cnick")));
 		}
