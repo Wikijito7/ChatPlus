52fd43414eb5304abbec3658fc99a61744509a09
Wikijito7 <wikyfg@gmail.com>
2016-06-04 16:55:31 +0200

    Some formatting

--- src/es/wiky/chat/Mensajes.java
+++ src/es/wiky/chat/Mensajes.java
@@ -17,6 +17,7 @@ public class Mensajes {
 	public static String successful_change_nick = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.successful_change_nick"));
 	public static String nick_changed = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.nick_changed"));
 	public static String player_dont_exist = ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.player_dont_exist"));
+	public static String usage_remove = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.usage_remove"));
 	public static String nick_set_usage = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.nick_set_usage"));
 	public static String playerSetReciever(String name){
 		return ChatColor.translateAlternateColorCodes('&',Main.clang.getString("lang.player_set_reciever").replaceAll("%NICK%", Main.user.getString("Users." + name + ".cnick")));
@@ -30,17 +31,10 @@ public class Mensajes {
 	public String playerDisplayName(String name){
 		return ChatColor.translateAlternateColorCodes('&',this.plugin.getConfig().getString("format").replaceAll("%DISPLANAME%", name));
 		}
-	public static String help_change = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.nick_set_usage"));
-	public static String help_set = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.nick_set_usage"));
-	public static String help_remove = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.nick_set_usage"));
-	public static String help_admin = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.nick_set_usage"));
-	public static String help_motd = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.nick_set_usage"));
-	
-
-	
-	
-	
-	
-	
-	
+	public static String help_change = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.help_change"));
+	public static String help_set = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.help_set"));
+	public static String help_remove = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.help_remove"));
+	public static String help_admin = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.help_admin"));
+	public static String help_motd = ChatColor.translateAlternateColorCodes('&', Main.clang.getString("lang.help_motd"));
+		
 }
