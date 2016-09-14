e90201bfdf9d5db26e5b3fabd781a2f8a30d7d06
Cadox8 <cadox8@gmail.com>
2016-09-14 16:58:48 +0200

    Added support for 1.10.2

--- src/es/wiky/chat/Cnick.java
+++ src/es/wiky/chat/Cnick.java
@@ -3,7 +3,6 @@ package es.wiky.chat;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,133 +10,127 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
 
 public class Cnick implements CommandExecutor {
-	private Main plugin;
-	public Cnick(Main main) {
-	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		Player p = (Player) sender;
 		if(cmd.getName().equalsIgnoreCase("cnick")){
 			if(args.length == 0){
 				p.sendMessage(Mensajes.help);
 			}
 			if(args.length == 1){
-			  if(args[0].equalsIgnoreCase("change") || args[0].equalsIgnoreCase("set")){		
-				if(args[0].equalsIgnoreCase("change")){
-					if(p.hasPermission("chatplus.change")){
-					p.sendMessage(Mensajes.nick_change_usage);
+				if(args[0].equalsIgnoreCase("change") || args[0].equalsIgnoreCase("set")){
+					if(args[0].equalsIgnoreCase("change")){
+						if(p.hasPermission("chatplus.change")){
+							p.sendMessage(Mensajes.nick_change_usage);
 						}else{
 							p.sendMessage(Mensajes.noperm);
 						}
 					}
-				if(args[0].equalsIgnoreCase("set")){
-					if(p.hasPermission("chatplus.set")){
-					p.sendMessage(Mensajes.nick_set_usage);
-						}else{
-						p.sendMessage(Mensajes.noperm);	
-							}
-						}
-			  		}else{
-			  			p.sendMessage(Mensajes.help);
-			  		}
-			}
-			if(args.length == 2){
-				if(args[0].equalsIgnoreCase("change") || args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")){	
-						if(args[0].equalsIgnoreCase("change")){
-							if(p.hasPermission("chatplus.change")){
-							if(Main.user.getString("Users." + p.getName() + ".cnick").equalsIgnoreCase("none")) {
-							Main.user.set("Users." + p.getName() + ".cnick", args[1]);
-							
-							p.sendMessage(Mensajes.successful_change_nick + " " + Mensajes.playerNewNick(p.getName()));
-							try {
-				                Main.user.save(Main.users);
-				                Main.user.load(Main.users);
-				            } catch (IOException | InvalidConfigurationException e) {
-				                e.printStackTrace();
-				            	}
-							}else{
-								p.sendMessage(Mensajes.nick_changed);
-							 	}
-							p.setDisplayName(Main.user.getString("Users." + p.getName() + ".cnick"));
-							
-							}else{
-								p.sendMessage(Mensajes.noperm);
-								}
-						}
-						
-						
 					if(args[0].equalsIgnoreCase("set")){
 						if(p.hasPermission("chatplus.set")){
-						p.sendMessage(Mensajes.nick_set_usage);
-						   }else{
-							   p.sendMessage(Mensajes.noperm);
-						 }
-					}
-				
-					if(args[0].equalsIgnoreCase("remove")){
-						if(p.hasPermission("chatplus.remove")){
-						Player pl = Bukkit.getPlayerExact(args[1]);
-						if(pl != null){
-						Main.user.set("Users." + pl.getName() + ".cnick", "none");
-						try {
-			                Main.user.save(Main.users);
-			                Main.user.load(Main.users);
-			            } catch (IOException | InvalidConfigurationException e) {
-			                e.printStackTrace();
-			            	}
-						pl.sendMessage(Mensajes.playerRemovedNick(pl.getName()));
-						p.sendMessage(Mensajes.nick_remove);
-								}else{
-									p.sendMessage(Mensajes.player_dont_exist);
-								}
+							p.sendMessage(Mensajes.nick_set_usage);
 						}else{
 							p.sendMessage(Mensajes.noperm);
 						}
 					}
-					}else{
+				}else{
 					p.sendMessage(Mensajes.help);
-					}
 				}
-			
-			
+			}
+			if(args.length == 2){
+				if(args[0].equalsIgnoreCase("change") || args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")){
+					if(args[0].equalsIgnoreCase("change")){
+						if(p.hasPermission("chatplus.change")){
+							if(Main.user.getString("Users." + p.getName() + ".cnick").equalsIgnoreCase("none")){
+								Main.user.set("Users." + p.getName() + ".cnick", args[1]);
+
+								p.sendMessage(Mensajes.successful_change_nick + " " + Mensajes.playerNewNick(p.getName()));
+								try{
+									Main.user.save(Main.users);
+									Main.user.load(Main.users);
+								}catch(IOException | InvalidConfigurationException e){
+									e.printStackTrace();
+								}
+							}else{
+								p.sendMessage(Mensajes.nick_changed);
+							}
+							p.setDisplayName(Main.user.getString("Users." + p.getName() + ".cnick"));
+
+						}else{
+							p.sendMessage(Mensajes.noperm);
+						}
+					}
+
+					if(args[0].equalsIgnoreCase("set")){
+						if(p.hasPermission("chatplus.set")){
+							p.sendMessage(Mensajes.nick_set_usage);
+						}else{
+							p.sendMessage(Mensajes.noperm);
+						}
+					}
+
+					if(args[0].equalsIgnoreCase("remove")){
+						if(p.hasPermission("chatplus.remove")){
+							Player pl = Bukkit.getPlayerExact(args[1]);
+							if(pl != null){
+								Main.user.set("Users." + pl.getName() + ".cnick", "none");
+								try{
+									Main.user.save(Main.users);
+									Main.user.load(Main.users);
+								}catch(IOException | InvalidConfigurationException e){
+									e.printStackTrace();
+								}
+								pl.sendMessage(Mensajes.playerRemovedNick(pl.getName()));
+								p.sendMessage(Mensajes.nick_remove);
+							}else{
+								p.sendMessage(Mensajes.player_dont_exist);
+							}
+						}else{
+							p.sendMessage(Mensajes.noperm);
+						}
+					}
+				}else{
+					p.sendMessage(Mensajes.help);
+				}
+			}
+
 			if(args.length == 3){
 				if(args[0].equalsIgnoreCase("set")){
 					if(p.hasPermission("chatplus.set")){
-					Player pl = Bukkit.getPlayerExact(args[1]);
-					if(pl != null){
-						if(args[2].equalsIgnoreCase("none")){
-							pl.setDisplayName(pl.getName());
-							p.sendMessage(Mensajes.playerSet(p.getName()));
-							pl.sendMessage(Mensajes.playerSetReciever(pl.getName()));
-						}else{
-						Main.user.set("Users." + pl.getName() + ".cnick", args[2]);
-						try {
-			                Main.user.save(Main.users);
-			                Main.user.load(Main.users);
-			            } catch (IOException | InvalidConfigurationException e) {
-			                e.printStackTrace();
-			            	}
-						p.sendMessage(Mensajes.playerSet(p.getName()));
-						pl.sendMessage(Mensajes.playerSetReciever(pl.getName()));
-						pl.setDisplayName(Main.user.getString("Users." + pl.getName() + ".cnick"));	
+						Player pl = Bukkit.getPlayerExact(args[1]);
+						if(pl != null){
+							if(args[2].equalsIgnoreCase("none")){
+								pl.setDisplayName(pl.getName());
+								p.sendMessage(Mensajes.playerSet(p.getName()));
+								pl.sendMessage(Mensajes.playerSetReciever(pl.getName()));
+							}else{
+								Main.user.set("Users." + pl.getName() + ".cnick", args[2]);
+								try{
+									Main.user.save(Main.users);
+									Main.user.load(Main.users);
+								}catch(IOException | InvalidConfigurationException e){
+									e.printStackTrace();
+								}
+								p.sendMessage(Mensajes.playerSet(p.getName()));
+								pl.sendMessage(Mensajes.playerSetReciever(pl.getName()));
+								pl.setDisplayName(Main.user.getString("Users." + pl.getName() + ".cnick"));
 							}
-					}else{
-						p.sendMessage(Mensajes.player_dont_exist);
-					 		}
 						}else{
-							p.sendMessage(Mensajes.noperm);
+							p.sendMessage(Mensajes.player_dont_exist);
 						}
 					}else{
-						p.sendMessage(Mensajes.help);
+						p.sendMessage(Mensajes.noperm);
 					}
+				}else{
+					p.sendMessage(Mensajes.help);
 				}
+			}
 			if(args.length >= 4){
 				p.sendMessage(Mensajes.help);
-				}
-			}		
-			return false;
-
+			}
 		}
+		return false;
+
 	}
-	
+}
--- src/es/wiky/chat/Motd.java
+++ src/es/wiky/chat/Motd.java
@@ -1,48 +1,45 @@
 package es.wiky.chat;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Motd implements CommandExecutor{
+public class Motd implements CommandExecutor {
 
-	
-	
 	private Main plugin;
 
-	public Motd(Main main) {
+	public Motd(Main main){
 		this.plugin = main;
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		Player p = (Player) sender;
 		if(cmd.getName().equalsIgnoreCase("motd")){
 			if(args.length == 0){
 				p.sendMessage(Mensajes.help);
 			}
-			
-		if(args.length == 1){
-			if(args[0].equalsIgnoreCase("see")){
-				if(p.hasPermission("chatplus.motd")){
-				p.sendMessage(org.bukkit.ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("motd.message")));
+
+			if(args.length == 1){
+				if(args[0].equalsIgnoreCase("see")){
+					if(p.hasPermission("chatplus.motd")){
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("motd.message")));
+					}else{
+						p.sendMessage(Mensajes.noperm);
+					}
 				}else{
-					p.sendMessage(Mensajes.noperm);
+					p.sendMessage(Mensajes.help);
 				}
-			}else{
+
+			}
+
+			if(args.length >= 2){
 				p.sendMessage(Mensajes.help);
 			}
-			
-		}
-			
-		if(args.length >= 2){
-			p.sendMessage(Mensajes.help);
-				}	
 		}
 		return false;
 	}
-		
-		
-		
+
 }
